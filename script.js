6b8a8f98dad02ad51b97a130af531e865c668789
Atulgupta0403 <atulgupta0403@gmail.com>
2024-05-08 14:05:34 +0530

    feedback and cards

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 const cursor = document.querySelector(".cursor");
 const container = document.querySelector("header");
 const anchor = document.querySelector(".anc");
-
+const nav = document.querySelector("navbar");
+const about = document.querySelector(".about");
 const img = document.querySelector(".nav img")
+
 // const image = document.querySelector(".nav .image")
-
-
 
 container.addEventListener("mousemove",(val)=>{
     cursor.style.left = val.x + "px";
     cursor.style.top = val.y + "px";
 })
 
-container.addEventListener("mousemove",()=>{
+nav.addEventListener("mousemove",()=>{
     img.classList.remove("image")
+    cursor.style.backgroundColor= "white"
+})
+
+about.addEventListener("mousemove",()=>{
+    img.classList.add("image")
+    cursor.style.backgroundColor= "black"
 })
