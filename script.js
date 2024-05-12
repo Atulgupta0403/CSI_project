8f4abd1cab2b9398f871c778eee3552b591e4602
Atulgupta0403 <atulgupta0403@gmail.com>
2024-05-12 22:51:02 +0530

    updated

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ const container = document.querySelector("header");
 const anchor = document.querySelector(".anc");
 const nav = document.querySelector("navbar");
 const about = document.querySelector(".about");
-const img = document.querySelector(".nav img")
+const img = document.querySelector(".nav img");
 
-// const image = document.querySelector(".nav .image")
 
 container.addEventListener("mousemove",(val)=>{
     cursor.style.left = val.x + "px";
@@ -20,4 +19,102 @@ nav.addEventListener("mousemove",()=>{
 about.addEventListener("mousemove",()=>{
     img.classList.add("image")
     cursor.style.backgroundColor= "black"
+})
+
+const tl = gsap.timeline()
+
+tl.from(".nav .name h1",{
+    y : -30,
+    opacity : 0 ,
+    duration : 1,
+    scale : 0.2
+})
+tl.from(".nav .anc a",{
+    y : -30,
+    opacity : 0 ,
+    duration : 1,
+    stagger : 0.3
+})
+tl.from(".left",{
+    x:-350,
+    opacity : 0,
+    duratin : 0.5,
+},"amin")
+tl.from(".right",{
+    x:350,
+    opacity : 0,
+    duratin : 0.5,
+},"amin")
+
+
+const contact  = document.querySelector(".nav .anc .contact")
+const cross = document.querySelector(".white i")
+
+const t2 = gsap.timeline()
+t2.to(".white",{
+    x:-460,
+    duration : 0.6
+})
+t2.from(".white .matter a",{
+    x:200,
+    opacity : 0,
+    duration : 0.3,
+    ease : "none",
+    stagger : 0.3
+})
+t2.from(".white i",{
+    opacity : 0,
+})
+
+t2.pause()
+
+contact.addEventListener("click",()=>{
+    t2.play()
+})
+
+cross.addEventListener("click",()=>{++
+    t2.reverse()
+})
+
+
+
+const car = document.querySelector(".about .car")
+gsap.to(car,{
+    x:1000,
+    y:400,
+    scrollTrigger:{
+        trigger:".about",
+        scroller : "body",
+        // markers : true,
+        // pin:true,
+        start : "top 10%",
+        end:"top -50%",
+        scrub : 2
+    }
+})
+
+const page2 = document.querySelector(".page2")
+const body = document.querySelector("body")
+
+window.addEventListener("wheel",(dets)=>{
+    if (dets.deltaY>=0) {
+        gsap.to(".scroller .scroll",{
+            transform : "translateX(-200%)",
+            duration : 2,
+            ease : "none"
+        })
+        gsap.to(".scroll i",{
+            rotate : 180
+        })
+    } else {
+        gsap.to(".scroller .scroll",{
+            transform : "translateX(00%)",
+            duration : 4,
+            ease : "none"
+
+        })
+        gsap.to(".scroll i",{
+            rotate : 0
+        })
+    }
 })
